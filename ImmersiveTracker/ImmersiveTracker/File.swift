ecfc785caedfc4b17bc37b79f6e6822a27197a1f
Yu Ho Kwok <will.kwok@invivointeractive.com>
2019-11-23 00:32:04 +0800

    Added Immersive Tracker iOS App Source Code

---
+++ ImmersiveTracker/ImmersiveTracker/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  VR Development Framework for Developing Industrial Training Simulation based on Smartphone
+//
+//  Created by Louis Tsui on 22/11/2019.
+//  Copyright © 2019 Jason Chow. All rights reserved.
+//
+
+import Foundation
