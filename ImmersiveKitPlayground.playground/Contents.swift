5e4444d357335082d81e92c529f3cbe300b18884
yuhokwok <yuho1985@gmail.com>
2019-12-11 21:03:46 +0800

    remove CocoaSyncSocket framework

---
+++ ImmersiveKitPlayground.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+var str = "Hello, pla，y一口路gr𣈱dd中。ab楽f...  "
+str = str.trimmingCharacters(in: CharacterSet.whitespaces)
+
+//print("\(str.components(separatedBy: CharacterSet.punctuationCharacters))")
+//print("\(str.rangeOfCharacter(from: CharacterSet.punctuationCharacters))")
+
+var arr = str.components(separatedBy: " ")
+if var last = arr.last {
+    let pattern =  "\\p{script=Han}+"
+    if let range = last.range(of: pattern, options: .regularExpression, range: nil, locale: nil) {
+        print("\(range)")
+        
+        
+        print(last[range.upperBound..<last.endIndex])
+        //last.removeSubrange(range)
+        //print(last)
+    } else {
+        print("no range")
+    }
+    
+    
+    
+    
+    /*
+    if let regex = try? NSRegularExpression(pattern: pattern, options: .allowCommentsAndWhitespace) {
+        let range = NSMakeRange(0, last.count)
+        //print("\()")
+        let results = regex.matches(in: last, options: [], range: range)
+        if let lastResult = results.last {
+            let range = lastResult.range
+            let index = last.index(last.startIndex, offsetBy: range.location + range.length)
+            let sub = last[index..<last.endIndex]
+            print("\(sub) loc:\(range.location) \(range.length)")
+            if sub.count > 0 {
+                //can go to engine
+            } else {
+                //last char is chinese
+            }
+        }
+    }
+     */
+    
+}
+print("\(arr)")
+
+
+
+func extract(input : String) -> String {
+    let pattern =  "\\p{script=Han}|\\p{InCJK_Symbols_and_Punctuation}+"
+    var str = input //"Hello, playgr𣈱ddd...  "
+    str = str.trimmingCharacters(in: CharacterSet.whitespaces)
+    
+    var theStr = str
+    var arr = str.components(separatedBy: " ")
+    if let last = arr.last {
+        theStr = last
+    }
+    
+    if var range = theStr.range(of: pattern, options: .regularExpression, range: nil, locale: nil) {
+        while let newRange = theStr.range(of: pattern, options: .regularExpression, range: range.upperBound..<theStr.endIndex, locale: nil) {
+            range = newRange
+        }
+        return String(theStr[range.upperBound..<theStr.endIndex])
+    } else {
+        theStr = str
+    }
+    return theStr
+}
+
+print("\(extract(input: str))")
+
+
---
+++ ImmversiveKit/ImmersiveKit/networkIO/ImmserviceNetworkCore.swift
@@ -0,0 +1,122 @@
+//
+//  ImmserviceNetworkCore.swift
+//  ImmersiveKit
+//
+//  Created by Yu Ho Kwok on 11/12/2019.
+//  Copyright © 2019 ImmersiveKit Team. All rights reserved.
+//
+
+import Foundation
+//import CocoaAsyncSocket
+import ARKit
+
+
+
+/// Foundation Class for ImmersiveClient & ImmersiveServver
+public class ImmersiveNetworkCore : NSObject, GCDAsyncSocketDelegate, NetServiceDelegate, NetServiceBrowserDelegate {
+    public var receiverDelegate : ImmersiveBodyReceiverDelegate?
+    
+    public var debugDelegate : ImmersiveKitDebug?
+    
+    /// A socket for connection to server / client
+    public var asyncSocket : GCDAsyncSocket?
+    
+    /// NetService Object for bonjour connection
+    public var netService : NetService?
+    
+    public init(runQueue: DispatchQueue?){
+        super.init()
+        let queue = runQueue ?? DispatchQueue.main
+        self.asyncSocket = GCDAsyncSocket(delegate: self, delegateQueue: queue)
+    }
+
+    /// Transmit the detected ARBodyAnchor to receiver
+    /// - Parameter body: ARBbodyAnchor detected by ARKit
+    public func transmit(body : ARBodyAnchor) {
+        
+    }
+    
+    
+    public func sendMessage(msg : String) {
+        if let data = msg.data(using: .utf8) {
+            sendData(data: data)
+        }
+        //printLog("send - \(msg)")
+    }
+    
+    public func sendData(data : Data) {
+        self.asyncSocket?.write(data, withTimeout: -1, tag: -1)
+        self.asyncSocket?.write(GCDAsyncSocket.crlfData(), withTimeout: -1, tag: -1)
+        self.asyncSocket?.readData(to: GCDAsyncSocket.crlfData(), withTimeout: -1, tag: -1)
+    }
+    
+    internal func printLog(_ msg : String) {
+        debugDelegate?.report(msg: msg)
+    }
+}
+
+//MARK: - GCDAsyncSocketDelegate
+extension ImmersiveNetworkCore {
+    public func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
+        
+    }
+    
+    public func socketDidDisconnect(_ sock: GCDAsyncSocket, withError err: Error?) {
+        
+    }
+    
+    public func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
+        
+    }
+    
+    public func socket(_ sock: GCDAsyncSocket, didWriteDataWithTag tag: Int) {
+        
+    }
+    
+    //server
+    public func socket(_ sock: GCDAsyncSocket, didAcceptNewSocket newSocket: GCDAsyncSocket) {
+
+    }
+}
+
+//MARK: - NetServiceDelegate
+extension ImmersiveNetworkCore {
+    public func netServiceDidPublish(_ sender: NetService) {
+        
+    }
+    
+    public func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
+        
+    }
+    
+    public func netService(_ sender: NetService, didNotResolve errorDict: [String : NSNumber]) {
+        
+    }
+    
+    public func netServiceDidResolveAddress(_ sender: NetService) {
+        
+    }
+}
+
+//MARK: - NetServiceBrowserDelegate
+extension ImmersiveNetworkCore {
+    public func netServiceBrowserWillSearch(_ browser: NetServiceBrowser) {
+        
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
+        
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        
+    }
+    
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
+        
+    }
+    
+    public func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
+        
+    }
+}
