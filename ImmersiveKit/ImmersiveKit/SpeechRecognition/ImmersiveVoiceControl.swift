94db1104a0cdef9e962a0e8d6ffea24f6142b47e
chan CHAKFUNG <51903303+ccfchan@users.noreply.github.com>
2020-05-14 22:36:26 +0800

    Update ImmersiveKit VoiceCommand

--- ImmersiveKit/ImmersiveKit/SpeechRecognition/ImmersiveVoiceControl.swift
+++ ImmersiveKit/ImmersiveKit/SpeechRecognition/ImmersiveVoiceControl.swift
@@ -11,17 +11,35 @@ import Foundation
 import AVFoundation
 import Speech
 
+
+
+
+
+
+
 /// Simple Voice control class based on SpeechRecognizer
-open class ImmersiveVoiceControl: UIViewController, ImmersiveVoiceSpeechRecognizerDelegate {
+open class ImmersiveVoiceControl: UIViewController, ImmersiveVoiceSpeechRecognizerDelegate   {
+    
     
     public var voiceCommand : ImmersiveSpeechRecognizer?
     
+    
+    enum SpeechWords: String {
+        case tracking, 開始追蹤, 返回
+        case vr = "virtual reality", 開始虛擬實境
+        case setting, 設定
+    }
+    
+   
+    
     override open func viewDidLoad() {
         super.viewDidLoad()
+        
         let locale = Locale(identifier: "zh-hk")
-        self.voiceCommand = ImmersiveSpeechRecognizer(locale: locale, keyPhrases: ["ImmersiveVoiceControl (zh-hk)"])
+        self.voiceCommand = ImmersiveSpeechRecognizer(locale: locale, keyPhrases: ["tracking"])
         self.voiceCommand?.grandPermission()
         self.voiceCommand?.voiceCommandDelegate = self
+        self.voiceCommand?.startRecognition()
     }
     
     override open func viewDidDisappear(_ animated: Bool) {
@@ -29,7 +47,15 @@ open class ImmersiveVoiceControl: UIViewController, ImmersiveVoiceSpeechRecogniz
         super.viewDidDisappear(animated)
     }
     
-    //MARK: - ImmersiveVoiceSpeechRecognizerDelegate
-    open func voiceCommandDetected(str: String) {}
     
+    
+
+    
+    //MARK: - ImmersiveVoiceSpeechRecognizerDelegate
+    open func voiceCommandDetected(str: String){
+       if let speechWord = SpeechWords.init(rawValue: str.lowercased()){
+       print("match the SpeechWords - \(speechWord.rawValue)")
+     }
+  }
 }
+
